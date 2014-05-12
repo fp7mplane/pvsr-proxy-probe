ac42427dd82bed73b026feddafbd4a6ddce4e4ee
baxi-netv <balazs.szabo@netvisor.hu>
2014-05-12 09:52:37 +0200

    copyright

--- pvsr_proxy_service.py
+++ pvsr_proxy_service.py
@@ -1,4 +1,25 @@
-#!/usr/bin/env python
+#
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+# mPlane Protocol Reference Implementation
+# Information Model and Element Registry
+#
+# (c) 2013-2014 mPlane Consortium (http://www.ict-mplane.eu)
+#               Author: Balazs Szabo <balazs.szabo@netvisor.hu>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 import mplane.model
 import mplane.scheduler
